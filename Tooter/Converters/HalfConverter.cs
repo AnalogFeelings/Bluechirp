e60790d56f690107c2e2f430880cb55dfc66974d
Colin Kiama <colinkiama@gmail.com>
2019-04-05 00:53:42 +0100

    Added "Material FAB" style button

---
+++ Tooter/Converters/HalfConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Tooter.Converters
+{
+    class HalfConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double numToReturn = 0;
+            if (value is double number)
+            {
+                numToReturn = number / 2;
+            }
+
+            return numToReturn;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
