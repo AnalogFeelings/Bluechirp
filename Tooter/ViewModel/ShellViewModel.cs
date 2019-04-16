9a606f4e98fb76c6ed0191946ea5fdda5598302f
Colin Kiama <colinkiama@gmail.com>
2019-04-16 15:25:28 +0100

    Done with Beta App stucture. Time to add API in

--- Tooter/ViewModel/ShellViewModel.cs
+++ Tooter/ViewModel/ShellViewModel.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tooter.Commands;
 using Tooter.Model;
 using Tooter.Services;
 using Tooter.View;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Tooter.ViewModel
 {
@@ -13,7 +16,17 @@ namespace Tooter.ViewModel
     {
         public ShellViewModel()
         {
-            NavService.Instance.Navigate(typeof(TimelineView), new HomeViewModel());
+            NewTootCommand = new RelayCommand(() => NavigateToTootView());
         }
+
+        private void NavigateToTootView()
+        {
+            NavService.CreateInstance(Window.Current.Content as Frame);
+            NavService.Instance.Navigate(typeof(TootView));
+        }
+
+        public RelayCommand NewTootCommand;
+
+        
     }
 }
