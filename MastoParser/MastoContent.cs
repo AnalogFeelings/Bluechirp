8f82531c06102e4ee33915e9c83a3f891b42b341
Colin Kiama <colinkiama@gmail.com>
2019-08-03 10:07:35 +0100

    Created initial starting classes and enums

---
+++ MastoParser/MastoContent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastoParser
+{
+    public class MastoContent
+    {
+        public string Content { get; set; }
+        public MastoContentType ContentType { get; set; }
+
+        public MastoContent(string content, MastoContentType contentType)
+        {
+            Content = content;
+            ContentType = contentType;
+        }
+    }
+}
---
+++ MastoParser/MastoText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastoParser
+{
+    public class MastoText
+    {
+        public bool IsParagraph { get; set; }
+        public string Content { get; set; }
+
+        public MastoText(string content, bool isParagraph = false)
+        {
+            Content = content;
+            IsParagraph = isParagraph;
+        }
+    }
+}
