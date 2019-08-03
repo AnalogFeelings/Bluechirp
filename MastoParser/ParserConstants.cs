814071bacd3dd03ab13bb2a4854abf0874971d36
Colin Kiama <colinkiama@gmail.com>
2019-08-03 09:56:51 +0100

    Creating parser for content

---
+++ MastoParser/ParserConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastoParser
+{
+    class ParserConstants
+    {
+        const char ParagraphTag = 'p';
+        const char LinkTag = 'a';
+        const char TagStartCharacter = '<';
+        const char TagEndCharacter = '>';
+        const char TagCloseCharaacter = '/';
+        const char AttributeValueCharacter = '"';
+        const char AttributeCharacter = '=';
+
+        const string BreakTag = "br";
+        const string SpanTag = "span";
+
+    }
+}
---
+++ MastoParser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastoParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter content to parse:");
+            string contentToParse = Console.ReadLine();
+
+            // Parse content
+        }
+    }
+}
