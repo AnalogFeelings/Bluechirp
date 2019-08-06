cc1058b16e60efff8e527d29835987dd5e200ee4
Colin Kiama <colinkiama@gmail.com>
2019-08-06 14:26:19 +0100

    Added New Console Playground

---
+++ MastoConsolePlayground/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MastoParserLib;
+
+namespace MastoConsolePlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
