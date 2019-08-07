1c89db3dbb17d26f5ca654c4bcfeaba5971ab39b
Colin Kiama <colinkiama@gmail.com>
2019-08-07 13:33:31 +0100

    Fixing up dialogs

--- Tooter/View/DialogTestView.xaml.cs
+++ Tooter/View/DialogTestView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Tooter.Dialogs;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,24 +14,24 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Tooter.Dialogs
+namespace Tooter.View
 {
-    public sealed partial class NewTootDialog : ContentDialog
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DialogTestView : Page
     {
-        public NewTootDialog()
+        public DialogTestView()
         {
             this.InitializeComponent();
-            
         }
 
-        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-        }
-
-        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
-        {
+            NewTootDialog dialog = new NewTootDialog();
+            await dialog.ShowAsync();
         }
     }
 }
