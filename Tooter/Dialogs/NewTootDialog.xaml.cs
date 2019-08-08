9dbe9635b5552f4e3d4ec09bb3b55f962a2947af
Colin Kiama <colinkiama@gmail.com>
2019-08-08 16:50:38 +0100

    NewTootDialog now binds to it's own ViewModel?

--- Tooter/Dialogs/NewTootDialog.xaml.cs
+++ Tooter/Dialogs/NewTootDialog.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Tooter.ViewModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -20,24 +21,14 @@ namespace Tooter.Dialogs
     public sealed partial class NewTootDialog : ContentDialog
     {
 
-
+        private NewTootViewModel _viewModel = new NewTootViewModel();
 
         public NewTootDialog()
         {
             this.InitializeComponent();
+            
         }
 
-        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
-        {
-        }
-
-        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
-        {
-        }
-
-        private void VisibilityOptionSelected(object sender, RoutedEventArgs e)
-        {
-
-        }
+        
     }
 }
---
+++ Tooter/ViewModel/NewTootViewModel.cs
@@ -0,0 +1,59 @@
+﻿using MastoParserLib.Model;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Tooter.ViewModel
+{
+    class NewTootViewModel : Notifier
+    {
+
+        private string statusContent;
+
+        public string StatusContent
+        {
+            get { return statusContent; }
+            set { statusContent = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        private Mastonet.Visibility _statustVisibilty;
+
+        public Mastonet.Visibility StatusVisibilty
+        {
+            get { return _statustVisibilty; }
+            set
+            {
+                _statustVisibilty = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        internal void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        internal void VisibilityOptionSelected(object sender, RoutedEventArgs e)
+        {
+            var selectedRadio = sender as RadioMenuFlyoutItem;
+            StatusVisibilty = ConvertTagToPostVisibility(selectedRadio.Tag);
+        }
+
+
+        private Mastonet.Visibility ConvertTagToPostVisibility(object tag)
+        {
+            var tagValue = (string)tag;
+            int tagAsNum = int.Parse(tagValue);
+            return (Mastonet.Visibility)tagAsNum;
+            
+        }
+    }
+}
