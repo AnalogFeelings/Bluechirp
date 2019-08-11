ec7ff7aa790c32fcfbb56f6ce35b91e6d59db11e
Colin Kiama <colinkiama@gmail.com>
2019-08-11 15:02:24 +0100

    Timestamps are formatted but not properly yet

---
+++ Tooter/Converters/SimpleTimestampConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tooter.Helpers;
+using Windows.UI.Xaml.Data;
+
+namespace Tooter.Converters
+{
+    class SimpleTimestampConverter : IValueConverter
+    {
+        const double roundedRatio = 0.15;
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string timestampString = null;
+
+            if (value is DateTime dateTimeToFormat)
+            {
+               timestampString = TimestampHelper.FormatTimestamp(dateTimeToFormat);
+            }
+
+            if (timestampString == null)
+            {
+                timestampString = string.Empty;
+            }
+            return timestampString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
