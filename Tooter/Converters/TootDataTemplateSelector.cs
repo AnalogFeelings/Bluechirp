5126acafab5b1d998212d6f8aa8916bc6995e41a
Colin Kiama <colinkiama@gmail.com>
2019-08-19 17:02:58 +0100

    Working on class that allows app to switch data templates on the fly

---
+++ Tooter/Converters/TootDataTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Tooter.Converters
+{
+    public class TootDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            return base.SelectTemplateCore(item);
+        }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            return base.SelectTemplateCore(item, container);
+        }
+
+    }
+}
