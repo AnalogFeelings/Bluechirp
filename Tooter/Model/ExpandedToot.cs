519385a02436ad033347e598f2437b2e52386019
Colin Kiama <colinkiama@gmail.com>
2019-08-19 19:04:42 +0100

    Redoing Expanded toot

--- Tooter/Model/ExpandedToot.cs
+++ Tooter/Model/ExpandedToot.cs
@@ -2,18 +2,43 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tooter.Model
 {
-    public class ExpandedToot
+    public class ExpandedToot: Status
     {
-        public Status StoredToot { get; set; }
-        
-        public ExpandedToot(Status tootToStore)
+
+        public ExpandedToot(Status expandedToot)
         {
-            StoredToot = tootToStore;
+            CopyPropertiesTo(expandedToot, this);
         }
+
+        public static void CopyPropertiesTo<T, TU>(T source, TU dest)
+        {
+            var sourceProps = typeof(T).GetProperties().Where(x => x.CanRead).ToList();
+            var destProps = typeof(TU).GetProperties()
+                    .Where(x => x.CanWrite)
+                    .ToList();
+
+            foreach (var sourceProp in sourceProps)
+            {
+                if (destProps.Any(x => x.Name == sourceProp.Name))
+                {
+                    var p = destProps.First(x => x.Name == sourceProp.Name);
+                    if (p.CanWrite)
+                    { // check if the property can be set or no.
+                        p.SetValue(dest, sourceProp.GetValue(source, null), null);
+                    }
+                }
+
+            }
+
+        }
+
+
+
     }
 }
---
+++ Tooter/ViewModel/ExpandedTootViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Mastonet.Entities;
+using MastoParserLib.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tooter.Model;
+using TooterLib.Helpers;
+
+namespace Tooter.ViewModel
+{
+    class ExpandedTootViewModel: Notifier
+    {
+        private Status _itemInContext = null;
+
+        private ObservableCollection<object> _contextTootItems;
+
+        public ObservableCollection<object> ContextTootItems
+        {
+            get { return _contextTootItems; }
+            set { _contextTootItems = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ExpandedTootViewModel()
+        {
+            ContextTootItems = new ObservableCollection<object>();
+        }
+
+        internal async Task AddInContextItems(ExpandedToot expandedToot)
+        {
+            _itemInContext = expandedToot.StoredToot;
+            var statusContext = await ClientHelper.Client.GetStatusContext(_itemInContext.Id);
+            
+
+        }
+
+    }
+}
