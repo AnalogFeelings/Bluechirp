aa4daadcf5eeaa30935468c9b2d807183ca573cc
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2023-04-29 00:41:03 +0200

    [Tests] Clean up TimestampTests.

--- Source/Bluechirp.Tests/TimestampTests.cs
+++ Source/Bluechirp.Tests/TimestampTests.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using Bluechirp.Library.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bluechirp.Tests
 {
@@ -14,71 +10,68 @@ namespace Bluechirp.Tests
         [TestMethod]
         public void TestIfHourFormattedCorrectly()
         {
-            const string expectedTimestamp = "1h";
+            string expectedTimestamp = "1h";
             DateTime pastTime = DateTime.Now.Subtract(new TimeSpan(1, 0, 0));
-
             string formattedTimestamp = TimestampHelper.FormatTimestamp(pastTime);
-            Assert.IsTrue(expectedTimestamp == formattedTimestamp);
+
+            Assert.AreEqual(expectedTimestamp, formattedTimestamp);
         }
 
         [TestMethod]
         public void TestIfMinsFormattedCorrectly()
         {
-            const string expectedTimestamp = "2m";
+            string expectedTimestamp = "2m";
             DateTime pastTime = DateTime.Now.Subtract(new TimeSpan(0, 2, 0));
-
             string formattedTimestamp = TimestampHelper.FormatTimestamp(pastTime);
-            Assert.IsTrue(expectedTimestamp == formattedTimestamp);
+
+            Assert.AreEqual(expectedTimestamp, formattedTimestamp);
         }
 
         [TestMethod]
         public void TestIfSecondsFormattedCorrectly()
         {
-            const string expectedTimestamp = "4s";
+            string expectedTimestamp = "4s";
             DateTime pastTime = DateTime.Now.Subtract(new TimeSpan(0, 0, 4));
-
             string formattedTimestamp = TimestampHelper.FormatTimestamp(pastTime);
-            Assert.IsTrue(expectedTimestamp == formattedTimestamp);
+
+            Assert.AreEqual(expectedTimestamp, formattedTimestamp);
         }
 
         [TestMethod]
         public void TestIfDaysFormattedCorrectly()
         {
-            const string expectedTimestamp = "7d";
-            DateTime pastTime = DateTime.Now.Subtract(new TimeSpan(7,0, 0, 0));
-
+            string expectedTimestamp = "7d";
+            DateTime pastTime = DateTime.Now.Subtract(new TimeSpan(7, 0, 0, 0));
             string formattedTimestamp = TimestampHelper.FormatTimestamp(pastTime);
-            Assert.IsTrue(expectedTimestamp == formattedTimestamp);
+
+            Assert.AreEqual(expectedTimestamp, formattedTimestamp);
         }
 
         [TestMethod]
         public void TestIfMonthsFormattedCorrectly()
         {
-            const string expectedTimestamp = "6m";
+            string expectedTimestamp = "6m";
+            string otherExpectedValue = "5m"; // Average month is 30 days but in reality, month days vary
 
-            // Average month is 30 days but in reality, month days vary
-            const string otherExpectedValue = "5m";
-            const int daysInMonth = 30;
+            int daysInMonth = 30;
+
             DateTime pastTime = DateTime.Now.Subtract(new TimeSpan(daysInMonth * 6, 0, 0, 0));
-
             string formattedTimestamp = TimestampHelper.FormatTimestamp(pastTime);
+
             Assert.IsTrue(expectedTimestamp == formattedTimestamp || otherExpectedValue == formattedTimestamp);
         }
 
         [TestMethod]
         public void TestIfYearsFormattedCorrectly()
         {
-            const string expectedTimestamp = "1y";
-            
-            // Sometimes you have leap years
-            const string otherExpectedValue = "11m";
-            
+            string expectedTimestamp = "1y";
+            string otherExpectedValue = "11m"; // Sometimes you have leap years
 
-            const int daysInYear = 365;
-            
+            int daysInYear = 365;
+
             DateTime pastTime = DateTime.Now.Subtract(new TimeSpan(daysInYear, 0, 0, 0));
-
             string formattedTimestamp = TimestampHelper.FormatTimestamp(pastTime);
+
             Assert.IsTrue(expectedTimestamp == formattedTimestamp || otherExpectedValue == formattedTimestamp);
         }
     }
