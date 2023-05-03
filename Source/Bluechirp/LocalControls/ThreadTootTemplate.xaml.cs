6b663e336f9976a09c5840b2550888f1ea452100
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2023-05-03 23:40:11 +0200

    [App -> Controls] Fix extremely dumb casting to List instead of .ToList().

--- Source/Bluechirp/LocalControls/ThreadTootTemplate.xaml.cs
+++ Source/Bluechirp/LocalControls/ThreadTootTemplate.xaml.cs
@@ -97,7 +97,7 @@ namespace Bluechirp.LocalControls
                             rootParagraph.Inlines.Add(run);
                         }
                         UpdateTootActions(reblogStatus);
-                        AddMediaToStatus((List<Attachment>)reblogStatus.MediaAttachments);
+                        AddMediaToStatus(reblogStatus.MediaAttachments.ToList());
                     }
                 }
 
@@ -121,7 +121,7 @@ namespace Bluechirp.LocalControls
                             rootParagraph.Inlines.Add(run);
                         }
                         UpdateTootActions(updatedStatus);
-                        AddMediaToStatus((List<Attachment>)updatedStatus.MediaAttachments);
+                        AddMediaToStatus(updatedStatus.MediaAttachments.ToList());
                     }
 
                 }
