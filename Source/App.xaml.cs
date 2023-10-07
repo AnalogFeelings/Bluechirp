97af8bc1a26f0432e8cad94b1220e96a38057022
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2023-10-08 01:44:49 +0200

    [General] Add project baseline.

---
+++ Source/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using AppActivationArguments = Microsoft.Windows.AppLifecycle.AppActivationArguments;
+using AppInstance = Microsoft.Windows.AppLifecycle.AppInstance;
+
+namespace Bluechirp
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        private Window appWindow;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override async void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            AppInstance mainInstance = AppInstance.FindOrRegisterForKey("main");
+
+            if (!mainInstance.IsCurrent)
+            {
+                AppActivationArguments appArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
+
+                await mainInstance.RedirectActivationToAsync(appArgs);
+
+                this.Exit();
+            }
+
+            appWindow = new MainWindow();
+            appWindow.Activate();
+        }
+    }
+}
