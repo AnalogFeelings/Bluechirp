be628e6263b28f321c3c7391ce10f2958fb2606d
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2023-12-13 19:37:16 +0100

    [Library -> ViewModel] Add basic shell view model.

--- Source/Bluechirp.Library/ViewModel/ShellViewModel.cs
+++ Source/Bluechirp.Library/ViewModel/ShellViewModel.cs
@@ -1,10 +1,20 @@
-﻿namespace Bluechirp.Library.ViewModel
+﻿using Bluechirp.Library.Helpers;
+using Bluechirp.Library.Services.Security;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Mastonet.Entities;
+
+namespace Bluechirp.Library.ViewModel
 {
     /// <summary>
     /// Implements a view model for a shell page.
     /// </summary>
-    public partial class ShellViewModel
+    public partial class ShellViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private Account _currentAccount;
+
+        private IAuthService _authService;
+
         public bool ShowDevBadge
         {
             get
@@ -15,6 +25,13 @@
                 return false;
 #endif
             }
+        }
+
+        public ShellViewModel(IAuthService authService)
+        {
+            _authService = authService;
+
+            _currentAccount = AsyncHelper.RunSync(async () => await _authService.Client.GetCurrentUser()) ;
         }
     }
 }
