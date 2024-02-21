f1675d3ba95b3253680220b0895a0cc679862f84
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2024-02-21 19:39:37 +0100

    [App -> Controls] Move timeline template to its own control.

---
+++ Source/Bluechirp/Controls/TootControl.xaml.cs
@@ -0,0 +1,33 @@
+using Bluechirp.Library.Models.View.Timelines;
+using Mastonet.Entities;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Bluechirp.Controls
+{
+    public sealed partial class TootControl : UserControl
+    {
+        public static readonly DependencyProperty StatusProperty = DependencyProperty.Register(nameof(Status),
+            typeof(Status), typeof(TootControl), null);
+
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(nameof(ViewModel),
+            typeof(BaseTimelineViewModel), typeof(TootControl), null);
+
+        public Status Status
+        {
+            get => (Status)GetValue(StatusProperty);
+            set => SetValue(StatusProperty, value);
+        }
+
+        public BaseTimelineViewModel ViewModel
+        {
+            get => (BaseTimelineViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        public TootControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
