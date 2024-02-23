ad5bb79d8940e28055175432fa235f5af352a99a
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2024-02-23 05:05:19 +0100

    [General] Add file license headers (#48).

--- Source/Bluechirp/Controls/TitleBar/TitleBar.WASDK.cs
+++ Source/Bluechirp/Controls/TitleBar/TitleBar.WASDK.cs
@@ -1,6 +1,20 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+#region License Information (GPLv3)
+// Bluechirp - A modern, native client for the Mastodon social media.
+// Copyright (C) 2023-2024 Analog Feelings and contributors.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#endregion
 
 using Bluechirp.Library.Interop;
 using Microsoft.UI;
